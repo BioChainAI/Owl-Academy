30f188cdd6893f2707c43ee2c4dc7062b234d8c7
Ebayednoob <ghoard1@gmail.com>
2026-06-13 12:01:08 -0700

    Create auth.js
    Signed-off-by: Ebayednoob <ghoard1@gmail.com>

---
+++ storage/logos/main/firebase/auth.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTcxIO9w3cvXDPrNP7mmdCMKLGNAPf4SA",
+  authDomain: "owl-academy-6bce2.firebaseapp.com",
+  projectId: "owl-academy-6bce2",
+  storageBucket: "owl-academy-6bce2.firebasestorage.app",
+  messagingSenderId: "337762500287",
+  appId: "1:337762500287:web:d024052bf39876947c100a"
+};
